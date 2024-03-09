54cfc0071bc21c8a326ac6bf46f48674320227c5
jonatan terrazas <jterrazas@gmail.com>
2024-03-09 07:05:08 -0400

    feat: add service findAllproducts

--- src/app/feature/delivery/product/product.component.ts
+++ src/app/feature/delivery/product/product.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { ProductRepository } from '../../domain/repository/product.repository';
+import { Product } from '../../domain/product';
 
 @Component({
   selector: 'app-product',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product.component.html',
   styleUrl: './product.component.css'
 })
 export class ProductComponent {
+  products: Product[];
+
+  constructor(@Inject('PRODUCT_REPOSITORY') private readonly productRepository: ProductRepository) {
+    this.products = this.productRepository.findAll();
+  }
 
 }
